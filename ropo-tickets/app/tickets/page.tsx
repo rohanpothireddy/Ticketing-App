2f32fcd95342b281dc6f8c421f50ed2b76eb9563
Rohan <pothireddyrohan@gmail.com>
2025-05-10 13:51:49 +0530

    server pagination added

--- ropo-tickets/app/tickets/page.tsx
+++ ropo-tickets/app/tickets/page.tsx
@@ -5,9 +5,22 @@ import Link from "next/link";
 import { buttonVariants } from "@/components/ui/button";
 import Pagination from "@/components/Pagination";
 
-const Tickets = async () => {
-  const tickets = await prisma.ticket.findMany();
-  console.log(tickets);
+interface SearchParams {
+  page: string;
+}
+
+const Tickets = async ({ searchParams }: { searchParams: SearchParams }) => {
+  const pageSize = 10;
+  const page = parseInt(searchParams.page) || 1;
+  const ticketCount = await prisma.ticket.count();
+
+
+  const tickets = await prisma.ticket.findMany({
+    take: pageSize,
+    skip: (page - 1) * pageSize,
+  });
+  
+
   return (
     <div>
       <Link
@@ -17,7 +30,11 @@ const Tickets = async () => {
         New Ticket
       </Link>
       <DataTable tickets={tickets} />
-      <Pagination itemCount={28} pageSize={10} currentPage={2} />
+      <Pagination
+        itemCount={ticketCount}
+        pageSize={pageSize}
+        currentPage={page}
+      />
     </div>
   );
 };
