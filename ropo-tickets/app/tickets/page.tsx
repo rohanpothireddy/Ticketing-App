3cfee7addd1c7604c147aa5b869e2b1676d89b89
Rohan <pothireddyrohan@gmail.com>
2025-05-10 12:53:48 +0530

    pagination component added

--- ropo-tickets/app/tickets/page.tsx
+++ ropo-tickets/app/tickets/page.tsx
@@ -3,6 +3,7 @@ import prisma from "@/prisma/db";
 import DataTable from "./DataTable";
 import Link from "next/link";
 import { buttonVariants } from "@/components/ui/button";
+import Pagination from "@/components/Pagination";
 
 const Tickets = async () => {
   const tickets = await prisma.ticket.findMany();
@@ -16,6 +17,7 @@ const Tickets = async () => {
         New Ticket
       </Link>
       <DataTable tickets={tickets} />
+      <Pagination itemCount={28} pageSize={10} currentPage={2} />
     </div>
   );
 };
